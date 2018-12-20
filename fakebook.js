0cfab3dc9c227d9eff1216639815a5f57c5d941e
calabaza91 <32208378+calabaza91@users.noreply.github.com>
2018-12-20 01:08:19 -0600

    Update js file
    Add more user objects in database array
    Add function to check for user in database
    SignIn() now checks isUserValid() returned boolean value

--- fakebook.js
+++ fakebook.js
@@ -1,9 +1,18 @@
 
 //Create a database with one user object
-var database = [{
-    username: "Caleb Cox",
+var database = [
+  {
+    username: "Caleb",
     password: "password1"
-}
+  },
+  {
+    username: "Michelle",
+    password: "pass"
+  },
+  {
+    username: "Grant",
+    password: "wubdub"
+  }
 ];
 
 //Create a newsfeed array of objects
@@ -22,14 +31,28 @@ var newsfeed = [
     }
 ];
 
-
+//Get username and password
 var usernamePrompt = prompt("What's your username?");
 var passwordPrompt = prompt("What's your password?");
 
-function signIn(username, password){
-  if(username === database[0].username && password === database[0].password){
-    console.log(newsfeed);
-  }else {
-    alert("Sorry, wrong username and/or password.");
+//Check for user in database
+function isUserValid (username, password){
+  for(var i = 0; i < database.length; i++){
+  if(username === database[i].username &&
+     password === database[i].password){
+    return true;
   }
 }
+return false;
+};
+
+//If user is in database, log in
+function signIn (username, password){
+  if(isUserValid(username, password)){
+    console.log(newsfeed);
+  }else{
+    alert("Sorry, wrong username and/or password.")
+  }
+};
+
+signIn(usernamePrompt, passwordPrompt);
