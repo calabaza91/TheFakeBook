76359ff590eaead0bee5a205ebd85a383b784ac6
calabaza91 <32208378+calabaza91@users.noreply.github.com>
2018-12-20 00:23:48 -0600

    initialize fakebook

---
+++ fakebook.js
@@ -0,0 +1,31 @@
+
+//Create a database with one user object
+var database = [{
+    username: "Caleb Cox",
+    password: "password1"
+}
+];
+
+//Create a newsfeed array of objects
+var newsfeed = [
+    {
+        username: "Jessica",
+        timeline: "Selfies!"
+    },
+      {
+        username: "Matt",
+        timeline: "Promotion!"
+    },
+      {
+        username: "Marge",
+        timeline: "Traveling!"
+    }
+];
+
+
+var usernamePrompt = prompt("What's your username?");
+var passwordPrompt = prompt("What's your password?");
+
+function signIn(username, password){
+
+}
