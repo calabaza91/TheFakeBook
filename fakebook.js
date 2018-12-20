208f5f39349ce3494a56e6cc012bb2bdbcb5e2c2
calabaza91 <32208378+calabaza91@users.noreply.github.com>
2018-12-20 00:27:00 -0600

    Finish sign in function

--- fakebook.js
+++ fakebook.js
@@ -27,5 +27,9 @@ var usernamePrompt = prompt("What's your username?");
 var passwordPrompt = prompt("What's your password?");
 
 function signIn(username, password){
-
+  if(username === database[0].username && password === database[0].password){
+    console.log(newsfeed);
+  }else {
+    alert("Sorry, wrong username and/or password.");
+  }
 }
